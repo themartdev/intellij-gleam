7ef6166a4fec5366e66e65e4e1f2411ff0b2137e
Simon Martineau <simon.martineau.dev@gmail.com>
2024-09-01 17:30:59 -0400

    refactor: (parser) distinguish tokens & parser types

--- src/main/kotlin/com/github/themartdev/intellijgleam/lang/psi/GleamType.kt
+++ src/main/kotlin/com/github/themartdev/intellijgleam/lang/psi/GleamType.kt
@@ -3,4 +3,4 @@ package com.github.themartdev.intellijgleam.lang.psi
 import com.github.themartdev.intellijgleam.lang.GleamLanguage
 import com.intellij.psi.tree.IElementType
 
-class GleamType(debugName: String) : IElementType(debugName, GleamLanguage)
+class GleamElementType(debugName: String) : IElementType(debugName, GleamLanguage)

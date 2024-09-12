8a9a315cd4f9692a6a4361a21e1cb2b0e3f14bf9
Simon Martineau <simon.martineau.dev@gmail.com>
2024-09-11 20:36:16 -0400

    feat: #6 LSP enable/disable

--- src/main/kotlin/com/github/themartdev/intellijgleam/ide/lsp/GleamLanguageServerFactory.kt
+++ src/main/kotlin/com/github/themartdev/intellijgleam/ide/lsp/GleamLanguageServerFactory.kt
@@ -1,16 +1,28 @@
 package com.github.themartdev.intellijgleam.ide.lsp
 
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
+import com.redhat.devtools.lsp4ij.LanguageServerEnablementSupport
 import com.redhat.devtools.lsp4ij.LanguageServerFactory
 import com.redhat.devtools.lsp4ij.server.StreamConnectionProvider
 
-val LOG = Logger.getInstance(GleamLanguageServer::class.java)
-
-class GleamLanguageServerFactory : LanguageServerFactory {
+class GleamLanguageServerFactory : LanguageServerFactory, LanguageServerEnablementSupport {
     override fun createConnectionProvider(project: Project): StreamConnectionProvider {
         val settings = GleamServiceSettings.getInstance(project)
         val gleamPath = settings.lspPath
         return GleamLanguageServer(gleamPath)
+    }
+
+    override fun isEnabled(project: Project): Boolean {
+        val settings = GleamServiceSettings.getInstance(project)
+        return settings.serviceMode == GleamLspMode.ENABLED
+    }
+
+    override fun setEnabled(enabled: Boolean, project: Project) {
+        val settings = GleamServiceSettings.getInstance(project)
+        if (enabled) {
+            settings.serviceMode = GleamLspMode.ENABLED
+        } else {
+            settings.serviceMode = GleamLspMode.DISABLED
+        }
     }
 }

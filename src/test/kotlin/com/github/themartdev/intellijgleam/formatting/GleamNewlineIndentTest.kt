129977aab4b1215931a8f633b3d7ecf91cac70d9
Simon Martineau <simon.martineau.dev@gmail.com>
2024-09-15 14:35:45 -0400

    refactor: add tests for newline indent

---
+++ src/test/kotlin/com/github/themartdev/intellijgleam/formatting/GleamNewlineIndentTest.kt
@@ -0,0 +1,79 @@
+package com.github.themartdev.intellijgleam.formatting
+
+import com.github.themartdev.intellijgleam.lang.GleamFileType
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixture4TestCase
+import org.junit.Test
+
+class GleamNewlineIndentTest : LightPlatformCodeInsightFixture4TestCase() {
+    override fun getTestDataPath() = "src/test/testData/formatting"
+
+    @Test
+    fun customTypeDefinition() {
+        val before = """
+            type MyType {<caret>}
+        """.trimIndent()
+
+        val after = """
+            type MyType {
+              <caret>
+            }
+        """.trimIndent()
+        testNewlineIndentation(before, after)
+    }
+
+    @Test
+    fun function() {
+        val before = """
+            fn my_func() {<caret>}
+        """.trimIndent()
+
+        val after = """
+            fn my_func() {
+              <caret>
+            }
+        """.trimIndent()
+        testNewlineIndentation(before, after)
+    }
+
+    @Test
+    fun caseExpression() {
+        val before = """
+            fn my_func() {
+                case x {<caret>}
+            }
+        """.trimIndent()
+
+        val after = """
+            fn my_func() {
+                case x {
+                  <caret>
+                }
+            }
+        """.trimIndent()
+        testNewlineIndentation(before, after)
+    }
+
+    @Test
+    fun blockExpression() {
+        val before = """
+            fn my_func() {
+                let x = {<caret>}
+            }
+        """.trimIndent()
+
+        val after = """
+            fn my_func() {
+                let x = {
+                  <caret>
+                }
+            }
+        """.trimIndent()
+        testNewlineIndentation(before, after)
+    }
+
+    private fun testNewlineIndentation(before: String, after: String) {
+        myFixture.configureByText(GleamFileType, before)
+        myFixture.type('\n')
+        myFixture.checkResult(after)
+    }
+}

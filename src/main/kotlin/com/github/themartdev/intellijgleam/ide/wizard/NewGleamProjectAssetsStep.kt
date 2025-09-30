0331aed6f1a8b9c2c6393a3b91b57564a7e842a7
Sage Vaillancourt <sage@sagev.space>
2025-09-30 14:04:31 -0400

    Basic project creation in non-Java IDEs
    Haven't figured out how to let users change the compilation target, but I think this is in a halfway-decent state.

--- src/main/kotlin/com/github/themartdev/intellijgleam/ide/wizard/NewGleamProjectAssetsStep.kt
+++ src/main/kotlin/com/github/themartdev/intellijgleam/ide/wizard/NewGleamProjectAssetsStep.kt
@@ -1,24 +1,18 @@
 package com.github.themartdev.intellijgleam.ide.wizard
 
 import com.intellij.ide.projectWizard.generators.AssetsNewProjectWizardStep
-import com.intellij.ide.wizard.NewProjectWizardStep
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.guessProjectDir
 
 class NewGleamProjectAssetsStep(private val parent: NewGleamProjectTargetStep) : AssetsNewProjectWizardStep(parent) {
+
     override fun setupAssets(project: Project) {
         setOutputDirectory(project.guessProjectDir()?.path!!)
-        val props = arrayOf(
-            Pair("gleamProjectName", project.name),
-            Pair("gleamProjectTarget", parent.target),
-        )
+        val props = GleamProjectAssets.assetProps(project.name, parent.target)
 
         if (context.isCreatingNewProject) {
-            addTemplateAsset("gleam.toml", "gleam.toml", *props)
-            addTemplateAsset(".gitignore", "gleam.gitignore", *props)
-
-            addEmptyDirectoryAsset("src")
-            addTemplateAsset("src/main.gleam", "main.gleam", *props)
+            GleamProjectAssets.assetDirs().forEach { dir -> addEmptyDirectoryAsset(dir) }
+            GleamProjectAssets.fileAssets().forEach { (sourcePath, templateName) -> addTemplateAsset(sourcePath, templateName, *props) }
         }
     }
 }
